316b1f980827074396d7f9d22be04a80ceaa1a67
Kyle E. Mitchell <kyle@kemitchell.com>
2019-11-04 15:23:55 -0800

    Improve PORT parsing

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,23 @@ async fn respond(request: Request<Body>) -> ServerResponse {
     Ok(response)
 }
 
+const DEFAULT_PORT: u16 = 8080;
+
 #[tokio::main]
 async fn main() {
-    let port_string = match env::var("PORT") {
-        Ok(val) => val,
-        Err(_) => String::from("8080"),
-    };
-    let port = match port_string.parse::<u16>() {
-        Ok(i) => i,
-        Err(_) => 8080,
+    let port;
+    let port_env_var = env::var("PORT");
+    match port_env_var {
+        Ok(port_string) => {
+            match port_string.parse::<u16>() {
+                Ok(i) => port = i,
+                Err(_) => {
+                    eprintln!("Error: Could not parse PORT environment variable.");
+                    std::process::exit(1);
+                }
+            };
+        }
+        Err(_) => port = DEFAULT_PORT,
     };
     let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
     let service = hyper::service::make_service_fn(|_| {
